b2fd17149738cab4dcce8fe0ea345790d0e5dd33
Lars <dev@falk-petersen.no>
2017-02-19 13:53:49 +0100

    Fix adjustment of unicode chars

--- ruter.py
+++ ruter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Copyright 2015 Lars Falk-Petersen.
+# Copyright 2015-2017 Lars Falk-Petersen <dev@falkp.no>.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
